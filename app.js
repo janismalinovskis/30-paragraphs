85f002dcc51e3ec813701d715e2680c2720c98ab
janismalinovskis <77164437+janismalinovskis@users.noreply.github.com>
2021-05-23 15:30:33 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+window.onload = klops;
+
+
+function klops() {
+  let i = 0;
+  for (i == 0; i < 30; i++) {
+  let text =  document.createElement("p");
+  text.innerHTML = "hello world";
+  document.body.appendChild(text);  
+
+}
+}
